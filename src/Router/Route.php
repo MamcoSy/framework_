99378c2cc69e3dd14abab087ca685e8d2c48a2b6
Th3-attacker <haje.malik5@gmail.com>
2021-03-03 23:03:26 +0000

    Route getter & setter

--- src/Router/Route.php
+++ src/Router/Route.php
@@ -1,21 +1,24 @@
 <?php
 
-declare ( strict_types = 1 );
+declare (strict_types = 1);
 
 namespace Hellium\Router;
 
 class Route implements RouteInterface
 {
     protected string $name;
+    protected string $path;
     protected  ? array $callback;
 
     /**
      * @param string $name
      */
-    public function __construct( string $name = '', ?array $callback = null )
+    public function __construct(string $path = '/', ?array $callback = null, string $name = '')
     {
         $this->name     = $name;
+        $this->path     = '/' . trim($path, '/');
         $this->callback = $callback;
+
     }
 
     /**
@@ -32,5 +35,31 @@ class Route implements RouteInterface
     public function getCallback(): ?array
     {
         return $this->callback;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function setCallback(array $callback): self
+    {
+        $this->callback = $callback;
+
+        return $this;
     }
 }
--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -15,20 +15,64 @@ class RouteTest extends TestCase
     {
         $route = $this->getRoute();
 
-        $this->assertInstanceOf( RouteInterface::class, $route );
+        $this->assertInstanceOf(RouteInterface::class, $route);
     }
 
     public function testGetRouteName()
     {
         $route = $this->getRoute();
 
-        $this->assertEquals( '', $route->getName() );
+        $this->assertEquals('', $route->getName());
     }
 
     public function testGetRouteCallback()
     {
         $route = $this->getRoute();
 
-        $this->assertEquals( null, $route->getCallback() );
+        $this->assertEquals(null, $route->getCallback());
+    }
+
+    public function testGetpath()
+    {
+        $route = $this->getRoute();
+
+        $this->assertEquals('/', $route->getPath());
+    }
+
+    public function testGetpathwithtwoslash()
+    {
+        $route = $this->getRoute();
+
+        $this->assertEquals('/', $route->getPath());
+    }
+
+    public function testSetPath()
+    {
+        $route = $this->getRoute();
+
+        $route->setPath('/home');
+
+        $this->assertEquals('/home', $route->getPath());
+    }
+
+    public function testSetName()
+    {
+        $route = $this->getRoute();
+
+        $route->setName('Mamadou');
+
+        $this->assertEquals('Mamadou', $route->getName());
+    }
+
+    public function testSetCallback()
+    {
+        $route = $this->getRoute();
+
+        $route->setCallback(['HomeController', 'index']);
+
+        $this->assertIsArray($route->getCallback());
+
+        $this->assertEquals(['HomeController', 'index'], $route->getCallback());
+
     }
 }
